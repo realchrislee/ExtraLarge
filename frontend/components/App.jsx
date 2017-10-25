783d46c204725681882cd3bcd156bd20c6a4383e
chris <chrislee92@gmail.com>
2017-10-25 17:47:40 -0400

    rename signin_container

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
 import SessionFormContainer from './session/session_form_container';
 import SignupContainer from './session/signup_container';
-import SigninContainer from './session/signin_container';
+import LoginContainer from './session/login_container';
 import { AuthRoute } from '../util/route_util';
 import { Route } from 'react-router-dom';
 
@@ -13,7 +13,7 @@ const App = () => (
       <GreetingContainer />
     </header>
 
-    <AuthRoute path="/login" component={SigninContainer} />
+    <AuthRoute path="/login" component={LoginContainer} />
     <AuthRoute path="/signup" component={SignupContainer} />
   </div>
 );
