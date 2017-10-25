868eb480cb9b450a8b50a5cddcd28c7b3a71fbc3
chris <chrislee92@gmail.com>
2017-10-25 09:55:10 -0400

    Add authroute and finished frontend auth

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
 import SessionFormContainer from './session/session_form_container';
+import { AuthRoute } from '../util/route_util';
 import { Route } from 'react-router-dom';
 
 const App = () => (
@@ -10,8 +11,8 @@ const App = () => (
       <GreetingContainer />
     </header>
 
-    <Route path="/login" component={SessionFormContainer} />
-    <Route path="/signup" component={SessionFormContainer} />
+    <AuthRoute path="/login" component={SessionFormContainer} />
+    <AuthRoute path="/signup" component={SessionFormContainer} />
   </div>
 );
 
