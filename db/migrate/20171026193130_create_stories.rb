9d5556d5302b69ce7fed3793de93df2255fdc5ad
chris <chrislee92@gmail.com>
2017-10-26 16:15:43 -0400

    Add story table

--- db/migrate/20171026193130_create_stories.rb
+++ db/migrate/20171026193130_create_stories.rb
@@ -1,11 +1,13 @@
 class CreateStories < ActiveRecord::Migration[5.1]
   def change
     create_table :stories do |t|
-      t.string :title
-      t.text :body
-      t.integer :author_id
+      t.string :title, null: false
+      t.text :body, null: false
+      t.integer :author_id, null: false
 
       t.timestamps
     end
+    add_index :stories, [:author_id], unique: true
+    add_index :stories, [:title]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025140814) do
+ActiveRecord::Schema.define(version: 20171026193130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stories", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_stories_on_author_id", unique: true
+    t.index ["title"], name: "index_stories_on_title"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
