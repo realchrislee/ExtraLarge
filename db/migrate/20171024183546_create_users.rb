eb26f770fae5de9ea470bf8c7c0490983d361e14
Chris <chrislee92@gmail.com>
2018-12-09 20:17:37 -0500

    fix migration

--- db/migrate/20171024183546_create_users.rb
+++ db/migrate/20171024183546_create_users.rb
@@ -8,6 +8,6 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.timestamps
     end
     add_index :users, :email, unique: true
-    add_index :users, :session_token, unique: true 
+    add_index :users, :session_token, unique: true
   end
 end
